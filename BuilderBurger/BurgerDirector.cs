a7e1429adbaa8d6ea1656920ec58d5d3f89680c9
Admin <Admin@DESKTOP-MAHVHFA>
2026-03-09 11:11:02 +0300

    BurgerDirector

---
+++ BuilderBurger/BurgerDirector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LR7_s8.BuilderBurger;
+
+namespace LR7_s8.BuilderBurger
+{
+    internal class BurgerDirector
+    {
+        private readonly IBurgerBuilder _builder;
+
+        public BurgerDirector(IBurgerBuilder builder)
+        {
+            _builder = builder;
+        }
+        public void BuildDefault()
+        {
+            _builder
+            .AddCheees()
+            .AddTomato()
+            .AddPickles();
+        }
+        public void BuildWithBeacon()
+        {
+            _builder
+            .AddTomato()
+            .AddBacon()
+            .AddLetuce();
+        }
+    }
+}
