73d4155163f4f25eb1b239503a444d0b8462d8f0
Admin <Admin@DESKTOP-MAHVHFA>
2026-03-09 11:02:41 +0300

    Добавьте файлы проекта.

---
+++ BuilderBurger/BuilderBurger.cs
@@ -0,0 +1,51 @@
+﻿using LR7_s8.DBCon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LR7_s8.DBCon;
+
+namespace LR7_s8.BuilderBurger
+{
+    public class BurgerBuilder : IBurgerBuilder
+    {
+        private Burgers _burger;
+
+        IBurgerBuilder IBurgerBuilder.AddBacon()
+        {
+            _burger.Bacon = true;
+            return this;
+        }
+
+        IBurgerBuilder IBurgerBuilder.AddCheees()
+        {
+            _burger.Cheese = true;
+            return this;
+        }
+
+        IBurgerBuilder IBurgerBuilder.AddLetuce()
+        {
+            _burger.Letuce = true;
+            return this;
+        }
+
+        IBurgerBuilder IBurgerBuilder.AddPickles()
+        {
+            _burger.Pickles = true;
+            return this;
+        }
+
+        IBurgerBuilder IBurgerBuilder.AddTomato()
+        {
+            _burger.Tomato = true;
+            return this;
+        }
+        public Burgers GetBurgers()
+        {
+            Burgers burger = _burger;
+            _burger = new Burgers();
+            return burger;
+        }
+    }
+}
---
+++ BuilderBurger/IBurgerBuilder.cs
@@ -0,0 +1,19 @@
+﻿using LR7_s8.DBCon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR7_s8.BuilderBurger
+{
+    internal interface IBurgerBuilder
+    {
+        IBurgerBuilder AddCheees();
+        IBurgerBuilder AddBacon();
+        IBurgerBuilder AddLetuce();
+        IBurgerBuilder AddTomato();
+        IBurgerBuilder AddPickles();
+        Burgers GetBurgers();
+    }
+}
